8f4b41dbd61d428daf73e83665f48edcac6abf7d
Ruoqi Huang <huangruoqi374@gmail.com>
2023-02-05 15:03:38 -0800

    change ui components

--- code/labeling_scene.py
+++ code/labeling_scene.py
@@ -1,6 +1,7 @@
 from UI_BASE.UI.scene import Scene
 from UI_BASE.UI.components.button import Button
 from UI_BASE.UI.components.text import Text
+from UI_BASE.UI.components.color_bar import ColorBar
 from UI_BASE.UI.sound import Channel
 from UI_BASE.UI.utils import IMAGE, SOUND
 
@@ -13,18 +14,19 @@ class LabelingScene(Scene):
             text = self.get("text")
             x, y = text.get_pos()
             text.set_pos(500 - x, 500 - y)
-        self.add("abc", Button(
-            text="abc",
-            text_fontsize=100,
-            x=100,
-            y=100,
-            on_click=on_click,
-        ))
+        # self.add("abc", Button(
+        #     text="abc",
+        #     text_fontsize=100,
+        #     x=100,
+        #     y=100,
+        #     on_click=on_click,
+        # ))
         self.add("text", Text(
             "something",
             x=200,
             y=300
         ))
+        self.add("progress_bar", ColorBar(100, 50, 100, 100))
 
     def update(self, delta_time, mouse_pos, clicked, pressed):
         super().update(delta_time, mouse_pos, clicked, pressed)
