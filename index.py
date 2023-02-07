96cd06520d9cc0ad658d6d678035cd926ca2afc2
Ruoqi Huang <huangruoqi374@gmail.com>
2023-02-06 23:21:01 -0800

    add dimension to config

--- index.py
+++ index.py
@@ -1,15 +1,13 @@
 from UI_BASE.UI.app import App
 from src.labeling_scene import LabelingScene
 import os
-from label_config import labels, video_name
+from label_config import labels, video_name, width, height
 
-VIDEO_WIDTH = 1280
-VIDEO_HEIGHT = 720
 
 app = App(
     LabelingScene,
-    VIDEO_WIDTH,
-    VIDEO_HEIGHT + 60,
+    width,
+    height + 60,
     video_path=os.path.join(os.curdir, "video", video_name),
     labels=labels,
 )
--- label_config.py
+++ label_config.py
@@ -1,6 +1,9 @@
 video_name = "test2.mp4"
+width=1280
+height=720
 labels = [
     "Nothing",
     "Nodding",
     "Shaking",
 ]
+
