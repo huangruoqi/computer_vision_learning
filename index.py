e55c7c1d5861309ecd0f5075a5fa4bb8f337212f
Ruoqi Huang <huangruoqi374@gmail.com>
2023-02-07 16:16:25 -0800

    change ui

--- index.py
+++ index.py
@@ -1,13 +1,15 @@
 from UI_BASE.UI.app import App
 from src.labeling_scene import LabelingScene
 import os
-from label_config import labels, video_name, width, height
+from label_config import labels, video_name, width, height, fps
 
 
 app = App(
     LabelingScene,
     width,
     height + 60,
+    fps=fps,
+    title="Video Labler",
     video_name=video_name,
     video_path=os.path.join(os.curdir, "video", video_name),
     labels=labels,
--- label_config.py
+++ label_config.py
@@ -1,6 +1,7 @@
 video_name = "test2.mp4"
 width=1280
 height=720
+fps=30
 
 # unlabeled data = "Others"
 labels = [
