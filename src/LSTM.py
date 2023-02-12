b18211aba1e1249b42a753bbcbd51203e1d2a295
Ruoqi Huang <huangruoqi374@gmail.com>
2023-02-11 22:20:45 -0800

    test data grouping

--- src/LSTM.py
+++ src/LSTM.py
@@ -5,6 +5,21 @@ import os
 
 df = pd.read_csv(os.path.join("data", "1676004101.mp4.csv"))
 
+groups = []
+
+current_group_label = None
+current_group = []
 for row in df.itertuples():
-    print(row.v20)
-    break
+    if current_group_label is None:
+        current_group_label = row.label
+    if current_group_label == row.label:
+        current_group.append(row)
+    else:
+        current_group_label = row.label
+        groups.append(current_group)
+        current_group = []
+
+if len(current_group):
+    groups.append(current_group)
+
+print(len(groups))
