030a6e80e544517ca150c1fcec591df9993a5738
Ruoqi Huang <huangruoqi374@gmail.com>
2023-04-01 08:43:10 -0700

    more thinking

--- src/test.py
+++ src/test.py
@@ -1,30 +1,51 @@
 class ModelTest:
     def __init__(self, base_model, raw_input, raw_output, options):
+        self.counter = 0
         self.base_model = base_model
         self.raw_input = raw_input
         self.raw_output = raw_output
-        self.options = options.items()
-        self.current_options = [None] * len(self.options)
-        self.final_options = {
-            'preprocess': [lambda a:a],
+        self.defalut_options = {
+            'preprocess': [],
+            'batch_size': 16,
+            'time_stamp': 32,
         }
-        for i in range(len(self.base_model.layers)):
-            self.final_options[f'layer{i}'] = [lambda a:a]
+        # for i in range(len(self.base_model.layers)):
+        #     self.final_options[f'layer{i}']
+        self.final_options = []
+        self.extend_options(options.items())
+        self.current_options = [None] * len(self.final_options)
+
+    def extend_options(self, options):
+        for option in options:
+            if isinstance(option[1], list):
+                self.extend_options(option[1])
+            else:
+                if callable(option[1][0]):
+                    self.final_options.append(option[1])
+                    self.counter+=1
 
     def process_options(self):
+        
+        
         pass
 
     def preprocess(self, func):
         return [func(row) for row in self.raw_input]
 
     def test(self, option_idx=0):
-        if option_idx==len(self.options):
+        if option_idx==len(self.final_options):
             self.build()
             pass
 
-        for i in self.options[option_idx]:
-            self.current_options[option_idx] = i
+        if callable(self.final_options[option_idx]):
+            self.current_options[option_idx] = False
             self.test(option_idx+1)
+            self.current_options[option_idx] = True
+            self.test(option_idx+1)
+        else:
+            for i in self.options[option_idx]:
+                self.current_options[option_idx] = i
+                self.test(option_idx+1)
 
     def build(self):
         self.process_options()
