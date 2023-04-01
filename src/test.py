3e7df867832311249296444efaf4b2e8e7d33b10
huangruoqi <44049919+huangruoqi@users.noreply.github.com>
2023-04-01 13:57:24 -0700

    Finish option parsing

--- src/test.py
+++ src/test.py
@@ -15,6 +15,11 @@ class ModelTest:
         self.expand_options(options.items())
         self.current_options = [None] * len(self.final_options)
 
+        self.final_inputs = None
+        self.final_outputs = None
+        self.final_params = None
+
+
     def expand_options(self, options, name=None):
         for option in options:
             if isinstance(option[1], list) and callable(option[1][0]):
@@ -28,12 +33,22 @@ class ModelTest:
                     
 
     def process_options(self):
+        self.final_inputs = self.raw_inputs
+        self.final_outputs = self.raw_outputs
+        self.final_params = {}
+        for i, (name, options) in enumerate(self.final_options):
+            option_idx = self.current_options[i]
+            if option_idx is None:
+                continue
+            option = options[option_idx]
+            if name == 'preprocess':
+                self.process(option, self.final_inputs)
+            elif name in ['batch_size', 'time_stamp']:
+                self.final_params[name] = option
         
-        
-        pass
 
-    def preprocess(self, func):
-        return [func(row) for row in self.raw_input]
+    def process(self, func, data):
+        return [func(row) for row in data]
 
     def test(self, option_idx=0):
         if option_idx==len(self.final_options):
@@ -53,4 +68,4 @@ class ModelTest:
 
     def build(self):
         self.process_options()
-        final_inputs = None
+        
